46e78a2cc8d092313cd3cd6c48650fbcf29d1f42
fuhongjuan <fuhongjuan@meituan.com>
2018-07-12 13:32:20 +0800

    remove svgo removeAttr plugin

--- bin/cli.js
+++ bin/cli.js
@@ -9,7 +9,7 @@ const colors = require('colors');
 const pkg = require('../package.json');
 
 // display the help text in red on the console
-const make_red = (text) => colors.red(txt);
+const makeRred = (text) => colors.red(text);
 
 // copy svg files from the project to new dir
 const copySvgFiles = (cmd, icondir) => {
@@ -33,15 +33,15 @@ const copySvgFiles = (cmd, icondir) => {
 };
 
 program
-  .version(pkg.version, '-v, --version')
+  .version(pkg.version, '-v, --version');
 
 program
   .command('clean <webpack.config.js path>')
   .option('-d, --dirname', 'assign the dir for store svg files, default is icons')
   .action(function (entryFile, options = {}) {
-    const basedir = process.cwd(); 
+    const basedir = process.cwd();
     const webpackConfigPath = path.resolve(basedir, entryFile);
-    const srcdir = path.join(path.dirname(webpackConfigPath), 'src'); 
+    const srcdir = path.join(path.dirname(webpackConfigPath), 'src');
     const iconDirname = options.dirname || 'icons';
     const dirs = fs.readdirSync(srcdir);
     const webpackConfig = require(webpackConfigPath);
@@ -50,17 +50,14 @@ program
         const entryPath = `${srcdir}/${entryName}`;
         const relativePath = path.relative(basedir, entryPath);
         const cmd = `git grep "ecom-icon-" ${relativePath}`;
-        const icondir = `${entryPath}/${iconDirname}`
+        const icondir = `${entryPath}/${iconDirname}`;
         utils.mkdir(icondir);
         copySvgFiles(cmd, icondir);
       });
-  })
+  });
 
-program.parse(process.argv)
+program.parse(process.argv);
 
 if (!process.argv.slice(2).length) {
-  program.outputHelp(make_red);
+  program.outputHelp(makeRred);
 }
-
-
-
